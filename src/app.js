d0f0f6c18d489e9f0c62bcb9398d55535d7e001a
Aditya Tiwari <adityatiwari0808@gmail.com>
2025-04-19 11:48:49 +0530

    changes done

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ app.get('/', (req,res)=>{
     res.send("hello server is running");
 });
 
+app.get('/hello', (req,res)=>{
+    res.send("hello is helling");
+})
+
 app.listen(3000, ()=>{
     console.log("server is running on localhost:3000");
 });
